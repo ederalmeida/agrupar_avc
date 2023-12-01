9039ea8676fa04c00095322b71ae8e2c4f109155
ederalmeida <eder.almeida@gmail.com>
2023-12-01 12:36:00 -0300

    v1.0.5

--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
 import cx_Freeze
-import hashlib
-import os.path
 
 build_exe_options = {
 'include_msvcr': True
@@ -11,7 +9,6 @@ exe = [cx_Freeze.Executable('agrupar_avc.py',
                             target_name = 'agrupar_avc.exe')]
 
 cx_Freeze.setup(name = 'agrupar_avc',
-                version=vl.v,
                 options = {'build_exe': build_exe_options},
                 executables = exe
 )
